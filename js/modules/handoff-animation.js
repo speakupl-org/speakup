22ce58eaa90efa5e481c81c0027df71fb426e5f2
lSpeakUP <lfangelomiranda@gmail.com>
2025-06-15 11:10:32 -0300

    Update handoff-animation.js

--- js/modules/handoff-animation.js
+++ js/modules/handoff-animation.js
@@ -1,4 +1,7 @@
 // js/modules/handoff-animation.js
+const gsap = window.gsap;
+const Flip = window.Flip;
+const MorphSVGPlugin = window.MorphSVGPlugin; // Make sure this is the correct name
 
 // The 'export' keyword is ADDED
 export function setupHandoffAnimation(elements, cube) {
