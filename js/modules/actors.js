ec52aeed1ddcc546c3a2f0be38d21a7f58b985a2
lSpeakUP <lfangelomiranda@gmail.com>
2025-06-15 11:45:20 -0300

    Update actors.js

--- js/modules/actors.js
+++ js/modules/actors.js
@@ -6,18 +6,7 @@ function createCubeActor(cube, appState, gsap) {
     const progress = appState.scrolly.progress;
 
     // --- THIS IS THE AREA TO EXPERIMENT WITH ---
-    // Change these lines to control the cube's Robust "Pass-it-Down" Fix**
-
-Let's refactor the code to follow the cleaner pattern we discussed. This will make the code more resilient and is good practice.
-
-1.  **Open `js/modules/actors.js`**.
-2.  Modify the file to look like this. I've added comments to show the changes.
-
-    ```javascript
-    // js/modules/actors.js
-
-    // The 'gsap' constant from the window is now passed in as an argument.
-    // We can remove `const gsap = window.gs animation
+    // Change these lines to control the cube's animation
     cube.rotation.y = progress * Math.PI * 2.5; // Turntable spin
     cube.rotation.x = progress * Math.PI * -1; // Tumble "down"
 
@@ -32,28 +21,7 @@ Let's refactor the code to follow the cleaner pattern we discussed. This will ma
 // This function ONLY controls the text pillars. It receives `gsap` as an argument.
 function createTextPillarActor(pillars, appState, gsap) {
     function update() {
-        const progress = appStateap;` from this file.
-
-    // CHANGE: Added 'gsap' as an argument
-    function createCubeActor(cube, appState, gsap) {
-      function update() {
         const progress = appState.scrolly.progress;
-        cube.rotation.y = progress * Math.PI * 2.5;
-        cube.rotation.x = progress * Math.PI * -1;
-        
-        const scale = (progress < 0.5)
-            ? gsap.utils.mapRange(0, 0.5, 1, 1.2, progress)
-            : gsap.utils.mapRange(0.5, 1, 1.2, 1, progress);
-        cube.scale.set(scale, scale, scale);
-      }
-      gsap.ticker.add(update);
-    }
-
-    // CHANGE: Added 'gsap' as an argument
-    function createTextPillarActor(pillars, appState, gsap) {
-        function update() {
-            const progress = appState.scrolly.progress;
-            .scrolly.progress;
         const numPillars = pillars.length;
         
         pillars.forEach((pillar, i) => {
