b477a75a9c42b501fddca76eea7f833ad5019cb3
lSpeakUP <lfangelomiranda@gmail.com>
2025-06-15 11:52:38 -0300

    Update scroll-engine.js

--- js/modules/scroll-engine.js
+++ js/modules/scroll-engine.js
@@ -1,7 +1,7 @@
 // js/modules/scroll-engine.js
 const gsap = window.gsap;
 
-export function createScrollEngine(triggerElement, onProgressUpdate) {
+export function createScrollEngine(triggerElement, onProgressUpdate, gsap) {
   gsap.timeline({
     scrollTrigger: {
       trigger: triggerElement,
@@ -10,7 +10,11 @@ export function createScrollEngine(triggerElement, onProgressUpdate) {
       scrub: 1.2,
       invalidateOnRefresh: true,
       onUpdate: (self) => {
+        // Use the callback to send the progress value out
         onProgressUpdate(self.progress);
+        
+        // ADD THIS PROBE:
+        console.log('Scroll Engine Firing! Progress:', self.progress.toFixed(2));
       },
     },
   });
