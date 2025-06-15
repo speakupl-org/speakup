82a80a834698f8e0f6d882225c86eaeb6be7a4be
lSpeakUP <lfangelomiranda@gmail.com>
2025-06-14 23:04:10 -0300

    Update scroll engine.js

--- js/modules/scroll engine.js
+++ js/modules/scroll engine.js
@@ -1,1 +1,15 @@
-
+// js/modules/scroll-engine.js
+export function createScrollEngine(triggerElement, onProgressUpdate) {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: triggerElement,
+      start: 'top top',
+      end: 'bottom bottom',
+      scrub: 1.2,
+      invalidateOnRefresh: true,
+      onUpdate: (self) => {
+        onProgressUpdate(self.progress);
+      },
+    },
+  });
+}
